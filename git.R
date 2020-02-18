00702cb964acdd11510c38a47ce4dccec710f9d0
davidrochha <60784309+davidrochha@users.noreply.github.com>
2020-02-18 11:32:19 -0300

    Update git.R

--- git.R
+++ git.R
@@ -1,7 +1,19 @@
+
+install.packages("dplyr")
+
+iris
 library(dplyr)
-
-
-
 glimpse(iris)
-select(iris, Sepal.Width)
-filter(iris, Sepal.Width>3)
+select(iris,Petal.Length)
+filter(iris,Petal.Length>2)
+iris %>% 
+  filter(Petal.Length>2) %>% 
+  mutate(Petal.Length=Petal.Length+2) %>% 
+  select(Species,Petal.Width)%>% 
+  glimpse()
+iris %>%
+  transmute(Petal=Petal.Length+2,Species)
+
+iris %>% 
+  group_by(Species) %>% 
+  summarise(iris)
